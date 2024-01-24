df38eb3c0d52adbcfc3d5326ef19d3b11423b85a
yinuo-noah-li <l.yinuo@wustl.edu>
2024-01-23 19:38:56 -0600

    Second Commit

---
+++ rock-paper-scissors/solution2.py
@@ -0,0 +1,16 @@
+with open("in.txt") as fin:
+    lines = fin.read().strip().split("\n")
+
+# X, Y, Z values represent offset in what to play
+play_map = {"A": 0, "B": 1, "C": 2, "X": -1, "Y": 0, "Z": 1}
+
+score_key = [1, 2, 3]
+
+score = 0
+for line in lines:
+    opp, result = [play_map[i] for i in line.split()]
+
+    score += (result + 1) * 3
+    score += score_key[(opp + result) % 3]
+
+print(score)
